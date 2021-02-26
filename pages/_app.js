02da6a5841afda44658c8f0b44e17b0911f9f5d1
WManzoli <willmanzoli@gmail.com>
2021-02-26 01:07:18 -0300

    fix: ios-font-import

--- pages/_app.js
+++ pages/_app.js
@@ -17,6 +17,7 @@ function MyApp({ Component, pageProps }) {
       <Component {...pageProps} />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"></link>
       <link rel="stylesheet" type="text/css" href="/nprogress.css" />
     </Layout>
   );
